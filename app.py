767450c69e918819e8e1479ce48906d7d68206c6
nikotpbn <niko@Nicolaus-MacBook-Pro.local>
2025-12-21 15:32:08 +0000

    input template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -6,7 +6,10 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def input_data(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="input.html")
