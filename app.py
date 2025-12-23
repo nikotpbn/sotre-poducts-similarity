db2da8c553bed1ecd5a941c7f36c0d431d3e6652
nikotpbn <niko@Nicolaus-MacBook-Pro.local>
2025-12-23 15:43:08 +0000

    output interface & input multiple individual files

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+
+from fastapi import FastAPI, Request, UploadFile
 
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -11,5 +12,11 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def input_data(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="input.html")
+    return templates.TemplateResponse(request=request, name="input.html")
+
+
+@app.post("/results", response_class=HTMLResponse)
+async def show_results(request: Request, files: list[UploadFile]):
+    print([file.filename for file in files])
+    ctx = {"results": "show results here"}
+    return templates.TemplateResponse(request=request, name="results.html", context=ctx)
