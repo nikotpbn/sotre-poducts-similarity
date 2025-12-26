78e614b02f05261f0bfb9f425890c8462142d302
nikotpbn <niko@Nicolaus-MacBook-Pro.local>
2025-12-26 12:17:34 +0000

    single file preprocessing

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 
+from engine.pre_process import read_file, clean_data, normalize
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
@@ -15,7 +17,11 @@ async def input_data(request: Request):
 
 @app.post("/results", response_class=HTMLResponse)
 async def show_results(request: Request, files: list[UploadFile], indices: list[int]):
-    print([file.filename for file in files])
-    print(indices)
+
+    single_df = read_file(files[0])
+    single_df = clean_data(single_df, indices[0])
+    single_df = normalize(single_df)
+    print(single_df)
+
     ctx = {"results": "show results here"}
     return templates.TemplateResponse(request=request, name="results.html", context=ctx)
---
+++ engine/pre_process.py
@@ -0,0 +1,75 @@
+import re
+import json
+import pandas as pd
+
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+# To suppress SettingWithCopyWarning
+pd.options.mode.copy_on_write = True
+
+# Source: https://countwordsfree.com/stopwords
+STOP_WORDS = json.load(open("engine/utils/stop_words_english.json"))
+
+
+def read_file(upload):
+    extension = upload.filename.split(".")[-1].lower()
+
+    try:
+        if extension == "csv":
+            df = pd.read_csv(upload.file)
+        elif extension == "xlsx":
+            df = pd.read_excel(upload.file, engine="openpyxl")
+        else:
+            raise ValueError("Unsupported file format")
+
+        return df
+
+    except Exception as e:
+        print(f"Error reading file: {e}")
+
+    return None
+
+
+def clean_data(dataframe, description_index):
+    headers = list(dataframe.columns)
+
+    # Drop all columns but description index
+    description_column = headers[description_index]
+    dataframe = dataframe.loc[:, dataframe.columns == description_column]
+
+    # Remove rows with any missing values
+    dataframe.dropna(axis="index", how="any", inplace=True)
+
+    # Remove duplicates
+    dataframe.drop_duplicates(subset=headers[description_index], inplace=True)
+
+    # Rename Columns to standard names
+    dataframe.rename(columns={description_column: "description"}, inplace=True)
+
+    return dataframe
+
+
+def normalize(dataframe):
+
+    # Lowercase the description column
+    dataframe["description"] = dataframe["description"].str.lower()
+
+    # # Remove punctuation from the description column
+    # # https://stackoverflow.com/a/50444347/7100120
+    # # re_sub performs better than pandas.replace
+    no_punctuation_regex = re.compile(r"[^\w\s]")
+    dataframe["description"] = [
+        no_punctuation_regex.sub("", desc) for desc in dataframe["description"]
+    ]
+
+    # remove numbers from the description column
+    no_number_regex = re.compile(r"\d+")
+    dataframe["description"] = [
+        no_number_regex.sub("", desc) for desc in dataframe["description"]
+    ]
+
+    # Remove stop words from the description column
+    dataframe["description"] = dataframe["description"].apply(
+        lambda x: " ".join([word for word in x.split() if word not in (STOP_WORDS)])
+    )
+
+    return dataframe
